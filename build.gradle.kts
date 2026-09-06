87323fd4954ef904d398140979a78f76390b3687
popoflooop-hash <popoflooop@gmail.com>
2026-09-06 05:45:20 +0300

    Update Gradle plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
