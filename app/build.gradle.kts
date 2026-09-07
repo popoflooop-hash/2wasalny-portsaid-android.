55d20c492327affcea9014ceb7d322261884a63f
popoflooop-hash <popoflooop@gmail.com>
2026-09-07 19:20:32 +0300

    Add Chrome Custom Tabs library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
+    // المكتبة الرسمية المعتمدة من جوجل لمصادقة الويب (Chrome Custom Tabs)
+    implementation("androidx.browser:browser:1.8.0")
+
     // مكتبات تسجيل الدخول السريع (Google Credential Manager)
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
